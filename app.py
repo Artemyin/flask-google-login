9f35b85c5d5d759bfe0225ac5c838fe3b8b7716b
Artemii <artemyin@gmail.com>
2023-05-14 15:53:27 +0200

    Imports, Configuration, and Setup

---
+++ app.py
@@ -0,0 +1,55 @@
+# python standard libraries
+import json
+import os
+import sqlite3
+
+
+# third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# internal imports
+from db import init_db_command
+from user import User
+
+
+# configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+
+# Flask app setup
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+# User session management setup
+# http://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_SECRET)
+
+
+# Flask-login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
