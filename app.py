c98d99bdb1cb3f63b7e6f9acaf7333c5f83ad7ee
Artemii <artemyin@gmail.com>
2023-05-14 16:11:03 +0200

    add homepage

--- app.py
+++ app.py
@@ -53,3 +53,17 @@ client = WebApplicationClient(GOOGLE_CLIENT_SECRET)
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
+
+
+@app.route("/"):
+def index():
+    if current_user.is_authenticated:
+        return (
+            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            "<div><p>Google Profile Picture:</p>"
+            '<img src="{}" alt="Google profile pic"></img></div>'
+            '<a class="button" href="/logout">Logout</a>'.format(
+                current_user.name, current_user.email, current_user.profile_pic
+        )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
